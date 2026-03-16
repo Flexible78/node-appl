3ab215a0310ffe3313829ed159e19513a5af1406
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-16 12:41:17 +0200

    asynchronous reading file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import "dotenv/config"
 import os from "node:os"
 import logger from "./logger.js"
-import fs from "node:fs"
-logger.info(`project directory is ${process.cwd()}`)
-const content = fs.readFileSync(process.cwd() + "src/logger.ts", {encoding: "ascii"})
+import fs from "node:fs/promises"
+const main = async () => {logger.info(`project directory is ${process.cwd()}`)
+const content = await fs.readFile(process.cwd() + "/src/logger.ts", {encoding: "ascii"})
 console.log(content) //printing whole file
 const lines: string[] = content.split('\n')
-logger.info(`first line of the file is ${lines[0]}`)
+logger.info(`first line of the file is ${lines[0]}`)}
+main()
