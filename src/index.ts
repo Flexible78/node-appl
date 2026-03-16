29c2246737db4f49ee467a8a8b908223443d2b0a
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-16 13:55:17 +0200

    split  code & comments task definition

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import "dotenv/config"
 import os from "node:os"
 import logger from "./logger.js"
-import fs, { writeFile } from "node:fs"
-const content: string = "שלום עולם"
-fs.writeFileSync("file.txt", content, {encoding: "utf8"})
-const readContent = fs.readFileSync("file.txt", {encoding: "utf8"})
-console.log(Array.from(readContent).reverse().join(""))
+
 
