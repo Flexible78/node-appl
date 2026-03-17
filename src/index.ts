9cad885541cc4eafe68c27d0fce0b79fb85e148f
Alexander <alexandertsyrkin@gmail.com>
2026-03-17 11:52:02 +0200

    fix: wire logger emitter and file output

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import "dotenv/config";
+import logger from "./logger.js";
 import LoggerEmitter from "./LoggerEmitter.js";
 import { consoleLogHandler } from "./consoleLogHandler.js";
 import { fileLogHandler } from "./fileLogHandler.js";
-const loggerEmitter = new LoggerEmitter();
-loggerEmitter.on("message", consoleLogHandler);
-loggerEmitter.on("message", fileLogHandler);
-loggerEmitter.log("Hello World!");
+
+const loggerEmitter = new LoggerEmitter([consoleLogHandler, fileLogHandler]);
+
+const messages: string[] = ["Hello world", "What's up", "How are you", "Bye"];
+
+messages.forEach(m => loggerEmitter.log(m));
