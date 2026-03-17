9eaae3eaaee6826f054afe93bb2c3e1a7f14015b
Alexander <alexandertsyrkin@gmail.com>
2026-03-17 11:11:51 +0200

    initial code of loggerEmitter

--- src/generator.ts
+++
@@ -1,7 +0,0 @@
-export function generateLotto(count: number, min: number, max: number): number[] {
-    const numbers = new Set<number>();
-    while (numbers.size <= count) {
-        numbers.add(Math.floor(Math.random() * (max - min + 1)) + min);
-    }
-    return Array.from(numbers);
-}
