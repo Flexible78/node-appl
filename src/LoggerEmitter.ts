67fdada1bccc1cc9afd137b5a0a2201f331d9938
Alexander <alexandertsyrkin@gmail.com>
2026-03-17 11:53:22 +0200

    chore: include logs and emitter changes

--- src/LoggerEmitter.ts
+++ src/LoggerEmitter.ts
@@ -1,7 +1,16 @@
 import { EventEmitter } from 'node:events';
 
 export default class LoggerEmitter extends EventEmitter {
+    constructor(handlers: ((message: string) => void)[] = []) {
+        super();
+        handlers.forEach(h => this.on("message", h));
+    }
+
     log(message: string): void {
         this.emit("message", message);
+    }
+
+    setHandler(handler: (message: string) => void) {
+        this.on("message", handler);
     }
 }
