e0a9fa24ac4c4ba8d0b4733bbb4a0557d6a0fe3f
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-18 16:10:23 +0200

    1000000000 numbers to file using pipe - not working due to small chunk

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
 import "dotenv/config";
 import logger from "./logger.js";
 import { createWriteStream } from "node:fs";
+import RandomNumbersStream from "./RandomNumbersStream.js";
 const writeStream = createWriteStream("large_file", {encoding: "utf8", highWaterMark: 1024 * 1024 * 10})
-const nChunks = 1_000_000
-const chunkNumbers = 1000
- const numbers: string = getRandomNumbers(chunkNumbers).join(" ");
-let i = 0;
-function writeNumbers() {
-  let canWrite = true;
-  
-  while (canWrite && i < nChunks) {
-    canWrite = writeStream.write(numbers)
-    i++;
-  }
-  if (i < nChunks) {
-      writeStream.once("drain",writeNumbers) //waiting for draining internal buffer
-  } else {
-     writeStream.end()
-  }
-  
- 
-}
-function getRandomNumbers(chunkNumbers: number): number[] {
-  return Array.from ({length: chunkNumbers}, () => Math.trunc(Math.random() * 1000))
-}
-console.time("write");
-writeStream.on("finish", () => {console.timeEnd("write"); })
-
-writeNumbers()
-
+console.time("pipe")
+new RandomNumbersStream(1_000_000_000).pipe(writeStream)
+writeStream.on("finish", ()=>console.timeEnd("pipe"))
 
 
